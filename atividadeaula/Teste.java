dae4c29720c78f72680f492e6e5a630d6e991e89
andrywlima <andrywmarcelo.lima@gmail.com>
2018-11-21 21:23:40 -0300

    fonn

---
+++ atividadeaula/Teste.java
@@ -0,0 +1,14 @@
+package atividadeaula;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		escreva ("hdfsfusdhfsd");
+	}
+		static void escreva(String texto) {
+			System.out.println(texto);
+		}
+
+	}
+
---
+++ atividadeaula/foon.java
@@ -0,0 +1,5 @@
+package atividadeaula;
+
+public class foon {
+
+}
